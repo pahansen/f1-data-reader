b18749cd7cabf480bf690d0445a9c554bd8fabf5
pahansen <39408669+pahansen@users.noreply.github.com>
2023-03-11 19:50:55 +0000

    use enum for function call

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,16 @@ mod parquet_writers {
     pub mod util_column_writer;
     pub mod writer_packet_car_telemetry_data;
 }
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use recorder::udp_recorder;
 use structs::parser;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    /// Function call that should be executed.
-    #[arg(long)]
-    function_call: String,
+struct Cli {
+    /// What mode to run the program in
+    #[arg(value_enum)]
+    mode: Mode,
     /// File path of F1 log.
     #[arg(long)]
     f1_log_file_path: String,
@@ -28,16 +28,27 @@ struct Args {
     parquet_file_path: Option<String>,
 }
 
-fn main() -> std::io::Result<()> {
-    let args = Args::parse();
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    /// Record f1 log
+    Recorder,
+    /// Parse recorded f1 log
+    Parser,
+}
 
-    if args.function_call == "record" {
-        println!("Recording f1 log...");
-        udp_recorder::record(&args.f1_log_file_path).unwrap();
-    } else if args.parquet_file_path.is_some() {
-        println!("Writing f1 log to parquet...");
-        let parquet_file_path = args.parquet_file_path.unwrap();
-        parser::parse_recorded_file(&args.f1_log_file_path, &parquet_file_path).unwrap();
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.mode {
+        Mode::Recorder => {
+            println!("Recording f1 log...");
+            udp_recorder::record(&cli.f1_log_file_path).unwrap();
+        }
+        Mode::Parser => {
+            println!("Writing f1 log to parquet...");
+            let parquet_file_path = cli.parquet_file_path.unwrap();
+            parser::parse_recorded_file(&cli.f1_log_file_path, &parquet_file_path).unwrap();
+        }
     }
     Ok(())
 }
