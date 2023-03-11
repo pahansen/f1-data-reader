047826b58f701a4554852be4a54b7926f1c8360f
pahansen <39408669+pahansen@users.noreply.github.com>
2023-03-11 19:23:37 +0000

    use clap for cmd line arguments

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,34 @@ mod parquet_writers {
     pub mod util_column_writer;
     pub mod writer_packet_car_telemetry_data;
 }
+use clap::Parser;
 use recorder::udp_recorder;
-use std::env;
 use structs::parser;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Function call that should be executed.
+    #[arg(long)]
+    function_call: String,
+    /// File path of F1 log.
+    #[arg(long)]
+    f1_log_file_path: String,
+    /// File path of parquet file.
+    #[arg(long)]
+    parquet_file_path: Option<String>,
+}
+
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let function_call = &args[1];
-    let f1_log_path = &args[2];
-    let parsed_file_path = &args[3];
-    if function_call == "record" {
+    let args = Args::parse();
+
+    if args.function_call == "record" {
         println!("Recording f1 log...");
-        udp_recorder::record(f1_log_path).unwrap();
-    } else {
+        udp_recorder::record(&args.f1_log_file_path).unwrap();
+    } else if args.parquet_file_path.is_some() {
         println!("Writing f1 log to parquet...");
-        parser::parse_recorded_file(f1_log_path, parsed_file_path).unwrap();
+        let parquet_file_path = args.parquet_file_path.unwrap();
+        parser::parse_recorded_file(&args.f1_log_file_path, &parquet_file_path).unwrap();
     }
     Ok(())
 }
