8a4d65e81cc50c5bcc5537538ff9054ac5610bd4
pahansen <39408669+pahansen@users.noreply.github.com>
2023-03-12 10:54:23 +0000

    format code

--- src/parquet_writers/util_column_writer.rs
+++ src/parquet_writers/util_column_writer.rs
@@ -1,29 +1,42 @@
-use parquet::{
-    data_type::{FloatType, Int32Type, BoolType, ByteArrayType, ByteArray}
-};
+use parquet::data_type::{BoolType, ByteArray, ByteArrayType, FloatType, Int32Type};
 use std::fs::File;
 
-pub fn write_float_column(row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>, column_data_vec: Vec<f32>, def_levels: Option<&[i16]>, rep_levels: Option<&[i16]>) {
+pub fn write_float_column(
+    row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>,
+    column_data_vec: Vec<f32>,
+    def_levels: Option<&[i16]>,
+    rep_levels: Option<&[i16]>,
+) {
     if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
         col_writer
-        .typed::<FloatType>()
-        .write_batch(&column_data_vec, def_levels, rep_levels)
-        .unwrap();
+            .typed::<FloatType>()
+            .write_batch(&column_data_vec, def_levels, rep_levels)
+            .unwrap();
         col_writer.close().unwrap()
     }
 }
 
-pub fn write_int32_column(row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>, column_data_vec: Vec<i32>, def_levels: Option<&[i16]>, rep_levels: Option<&[i16]>) {
+pub fn write_int32_column(
+    row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>,
+    column_data_vec: Vec<i32>,
+    def_levels: Option<&[i16]>,
+    rep_levels: Option<&[i16]>,
+) {
     if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
         col_writer
-        .typed::<Int32Type>()
-        .write_batch(&column_data_vec, def_levels, rep_levels)
-        .unwrap();
+            .typed::<Int32Type>()
+            .write_batch(&column_data_vec, def_levels, rep_levels)
+            .unwrap();
         col_writer.close().unwrap()
     }
 }
 
-pub fn write_u64_as_bytearray_column(row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>, column_data_vec: Vec<u64>, def_levels: Option<&[i16]>, rep_levels: Option<&[i16]>) {
+pub fn write_u64_as_bytearray_column(
+    row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>,
+    column_data_vec: Vec<u64>,
+    def_levels: Option<&[i16]>,
+    rep_levels: Option<&[i16]>,
+) {
     let mut u64_byte_array_vec = Vec::new();
     for value in column_data_vec {
         let u64_string = value.to_string();
@@ -33,19 +46,24 @@ pub fn write_u64_as_bytearray_column(row_group_writer: &mut parquet::file::write
     }
     if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
         col_writer
-        .typed::<ByteArrayType>()
-        .write_batch(&u64_byte_array_vec, def_levels, rep_levels)
-        .unwrap();
+            .typed::<ByteArrayType>()
+            .write_batch(&u64_byte_array_vec, def_levels, rep_levels)
+            .unwrap();
         col_writer.close().unwrap()
     }
 }
 
-pub fn write_bool_column(row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>, column_data_vec: Vec<bool>, def_levels: Option<&[i16]>, rep_levels: Option<&[i16]>) {
+pub fn write_bool_column(
+    row_group_writer: &mut parquet::file::writer::SerializedRowGroupWriter<File>,
+    column_data_vec: Vec<bool>,
+    def_levels: Option<&[i16]>,
+    rep_levels: Option<&[i16]>,
+) {
     if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
         col_writer
-        .typed::<BoolType>()
-        .write_batch(&column_data_vec, def_levels, rep_levels)
-        .unwrap();
+            .typed::<BoolType>()
+            .write_batch(&column_data_vec, def_levels, rep_levels)
+            .unwrap();
         col_writer.close().unwrap()
     }
 }
--- src/parquet_writers/writer_packet_car_telemetry_data.rs
+++ src/parquet_writers/writer_packet_car_telemetry_data.rs
@@ -1,17 +1,20 @@
+use crate::parquet_writers::util_column_writer::{
+    write_bool_column, write_float_column, write_int32_column, write_u64_as_bytearray_column,
+};
 use crate::structs::packet_car_telemetry_data::PacketCarTelemetryData;
 use crate::structs::packet_header::PacketHeader;
-use crate::parquet_writers::util_column_writer::{write_float_column, write_int32_column, write_bool_column, write_u64_as_bytearray_column};
 use binrw::BinRead;
-use std::{fs::{self, File}, path::Path, sync::Arc};
 use parquet::{
-    file::{
-        properties::WriterProperties,
-        writer::SerializedFileWriter,
-    },
-    schema::parser::parse_message_type
+    file::{properties::WriterProperties, writer::SerializedFileWriter},
+    schema::parser::parse_message_type,
+};
+use std::{
+    fs::{self, File},
+    path::Path,
+    sync::Arc,
 };
 
-pub fn new(file_path: &Path) -> SerializedFileWriter<File>{
+pub fn new(file_path: &Path) -> SerializedFileWriter<File> {
     let schema = "message schema {
         REQUIRED INT32 m_packet_format;
         REQUIRED BYTE_ARRAY m_session_uid;
@@ -29,13 +32,17 @@ pub fn new(file_path: &Path) -> SerializedFileWriter<File>{
         REQUIRED INT32 m_rev_lights_bit_value;
       }";
     let parsed_schema = Arc::new(parse_message_type(schema).unwrap());
-    
+
     let props = Arc::new(WriterProperties::builder().build());
     let parquet_file = fs::File::create(file_path).unwrap();
     SerializedFileWriter::new(parquet_file, parsed_schema, props).unwrap()
 }
 
-pub fn write(packet_header: &PacketHeader, mut file: &std::fs::File, writer: &mut SerializedFileWriter<File>) -> u64{
+pub fn write(
+    packet_header: &PacketHeader,
+    mut file: &std::fs::File,
+    writer: &mut SerializedFileWriter<File>,
+) -> u64 {
     let message = PacketCarTelemetryData::read(&mut file).unwrap();
     let len_car_telemetry = message.m_car_telemetry_data.len();
     let mut is_player_car_vec: Vec<bool> = vec![false; len_car_telemetry];
@@ -45,7 +52,7 @@ pub fn write(packet_header: &PacketHeader, mut file: &std::fs::File, writer: &mu
     let mut is_secondary_player_car_vec: Vec<bool> = vec![false; len_car_telemetry];
     if usize::from(packet_header.m_secondary_player_car_index) < len_car_telemetry {
         is_secondary_player_car_vec[usize::from(packet_header.m_player_car_index)] = true;
-    } 
+    }
     let mut m_speed_vec: Vec<i32> = Vec::new();
     let mut m_throttle_vec: Vec<f32> = Vec::new();
     let mut m_steer_vec: Vec<f32> = Vec::new();
@@ -66,25 +73,48 @@ pub fn write(packet_header: &PacketHeader, mut file: &std::fs::File, writer: &mu
         m_rev_lights_percent_vec.push(i32::from(car_telemetry.m_rev_lights_percent));
         m_rev_lights_bit_value_vec.push(i32::from(car_telemetry.m_rev_lights_bit_value));
     }
-    
+
     let mut row_group_writer = writer.next_row_group().unwrap();
-    write_int32_column(&mut row_group_writer, vec![i32::from(packet_header.m_packet_format); len_car_telemetry], None, None);
-    write_u64_as_bytearray_column(&mut row_group_writer, vec![packet_header.m_session_uid; len_car_telemetry], None, None);
-    write_float_column(&mut row_group_writer, vec![packet_header.m_session_time; len_car_telemetry], None, None);
+    write_int32_column(
+        &mut row_group_writer,
+        vec![i32::from(packet_header.m_packet_format); len_car_telemetry],
+        None,
+        None,
+    );
+    write_u64_as_bytearray_column(
+        &mut row_group_writer,
+        vec![packet_header.m_session_uid; len_car_telemetry],
+        None,
+        None,
+    );
+    write_float_column(
+        &mut row_group_writer,
+        vec![packet_header.m_session_time; len_car_telemetry],
+        None,
+        None,
+    );
     write_bool_column(&mut row_group_writer, is_player_car_vec, None, None);
-    write_bool_column(&mut row_group_writer, is_secondary_player_car_vec, None, None);
+    write_bool_column(
+        &mut row_group_writer,
+        is_secondary_player_car_vec,
+        None,
+        None,
+    );
     write_int32_column(&mut row_group_writer, m_speed_vec, None, None);
-    write_float_column(&mut row_group_writer, m_throttle_vec, None, None);    
+    write_float_column(&mut row_group_writer, m_throttle_vec, None, None);
     write_float_column(&mut row_group_writer, m_steer_vec, None, None);
     write_float_column(&mut row_group_writer, m_brake_vec, None, None);
     write_int32_column(&mut row_group_writer, m_clutch_vec, None, None);
     write_int32_column(&mut row_group_writer, m_engine_rpm_vec, None, None);
     write_int32_column(&mut row_group_writer, m_drs_vec, None, None);
     write_int32_column(&mut row_group_writer, m_rev_lights_percent_vec, None, None);
-    write_int32_column(&mut row_group_writer, m_rev_lights_bit_value_vec, None, None);
-    
-    
+    write_int32_column(
+        &mut row_group_writer,
+        m_rev_lights_bit_value_vec,
+        None,
+        None,
+    );
+
     row_group_writer.close().unwrap();
     1
 }
-
