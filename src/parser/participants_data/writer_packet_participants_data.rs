b69f156686c0016d72c1b497cbea4b6bc5c22240
pahansen <39408669+pahansen@users.noreply.github.com>
2023-03-27 18:20:56 +0000

    restructure participant data

--- src/parser/participants_data/writer_packet_participants_data.rs
+++ src/parser/participants_data/writer_packet_participants_data.rs
@@ -23,7 +23,7 @@ pub fn new(file_path: &Path) -> SerializedFileWriter<File> {
         REQUIRED FLOAT m_session_time;
         REQUIRED BOOLEAN is_player_car;
         REQUIRED BOOLEAN is_secondary_player_car;
-        REQUIRED INT32 participant_index;
+        REQUIRED INT32 car_index;
         REQUIRED BYTE_ARRAY m_name;
       }";
     let parsed_schema = Arc::new(parse_message_type(schema).unwrap());
@@ -39,16 +39,16 @@ pub fn write(
     writer: &mut SerializedFileWriter<File>,
 ) -> u64 {
     let message = PacketParticipantsData::read(&mut file).unwrap();
-    let len_participants = message.m_participants.len();
-    let mut is_player_car_vec: Vec<bool> = vec![false; len_participants];
-    if usize::from(packet_header.m_player_car_index) < len_participants {
+    let number_of_cars = message.m_participants.len();
+    let mut is_player_car_vec: Vec<bool> = vec![false; number_of_cars];
+    if usize::from(packet_header.m_player_car_index) < number_of_cars {
         is_player_car_vec[usize::from(packet_header.m_player_car_index)] = true;
     }
-    let mut is_secondary_player_car_vec: Vec<bool> = vec![false; len_participants];
-    if usize::from(packet_header.m_secondary_player_car_index) < len_participants {
+    let mut is_secondary_player_car_vec: Vec<bool> = vec![false; number_of_cars];
+    if usize::from(packet_header.m_secondary_player_car_index) < number_of_cars {
         is_secondary_player_car_vec[usize::from(packet_header.m_secondary_player_car_index)] = true;
     }
-    let participant_index: Vec<i32> = (0..22).collect();
+    let car_index: Vec<i32> = (0..22).collect();
     let mut m_name_vec: Vec<String> = Vec::new();
 
     for participant in message.m_participants {
@@ -60,19 +60,19 @@ pub fn write(
     let mut row_group_writer = writer.next_row_group().unwrap();
     write_int32_column(
         &mut row_group_writer,
-        vec![i32::from(packet_header.m_packet_format); len_participants],
+        vec![i32::from(packet_header.m_packet_format); number_of_cars],
     );
     write_u64_as_bytearray_column(
         &mut row_group_writer,
-        vec![packet_header.m_session_uid; len_participants],
+        vec![packet_header.m_session_uid; number_of_cars],
     );
     write_float_column(
         &mut row_group_writer,
-        vec![packet_header.m_session_time; len_participants],
+        vec![packet_header.m_session_time; number_of_cars],
     );
     write_bool_column(&mut row_group_writer, is_player_car_vec);
     write_bool_column(&mut row_group_writer, is_secondary_player_car_vec);
-    write_int32_column(&mut row_group_writer, participant_index);
+    write_int32_column(&mut row_group_writer, car_index);
     write_string_as_bytearray_column(&mut row_group_writer, m_name_vec);
 
     row_group_writer.close().unwrap();
